2b0169dac209085e3e1a774c7abcdc165071f812
Quentin90 <quentinoternaud@gmail.com>
2017-01-04 23:48:20 +0100

    Add Annonces et gestions d'erreur

--- App/Model/EvenementModel.php
+++ App/Model/EvenementModel.php
@@ -10,7 +10,7 @@ class EvenementModel {
     private $db;
 
     public function __construct(Application $app) {
-        $this->db = $app['db'];
+        $this->db = $app['db'];//
     }
     // http://docs.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/query-builder.html#join-clauses
     public function getAllEvenements()
