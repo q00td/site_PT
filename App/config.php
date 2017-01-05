5daaa220346e85c2f63758b2fd39749085a9370f
Yodome <van_minh_nguyen@hotmail.com>
2017-01-05 19:08:30 +0100

    Creation des evenements

--- App/config.php
+++ App/config.php
@@ -3,7 +3,7 @@ define("database","PT");
 define("hostname","localhost");
 //define("hostname","localhost");
 define("username","root");
-define("password","0389550045");  //JJMM
+define("password","jinn999*");  //JJMM
 
 define('BASE_URL', explode('app_dev.php', $_SERVER['SCRIPT_NAME'])[0]);
 //define("BASE_URL","http://votreURL/");
